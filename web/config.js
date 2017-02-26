c1d070861093a2cea7a93ae0822c5a2ecd2cc9f7
Koen <koen@koenvh.nl>
2017-02-26 23:14:38 +0100

    Fix PeerJS key

--- web/config.js
+++ web/config.js
@@ -12,7 +12,7 @@ var g_skinConfig = {
     },
     "whitenoise": true,                     //Whether to play static when a station is far away
     "transition-whitenoise": true,          //Whether to play static when changing stations
-    "peerJSkey": "damhc79361fhto6r",        //API key for PeerJS (remote control), no reason to change this yet
+    "peerJSkey": "w710twrvcnfdpldi",        //API key for PeerJS (remote control), no reason to change this yet
     "ets2": {
         "map": "cities-ets2.js",  //What map you're using, this is default + ProMods + RusMap + Project Balkans + Egypt add-on by default. Pick one of the files in the cities folder.
         "stations": "stations-europe.js",   //What stations file to use, currently only stations-europe available.
