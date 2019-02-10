036365fd0d9f791a294966bc4aca459e7896aead
Koen <koen@koenvh.nl>
2019-02-10 18:52:17 +0100

    Station updates

--- web/stations/stations-america.js
+++ web/stations/stations-america.js
@@ -916,29 +916,49 @@ var stations = {
     ],
     "eureka": [
         {
-            name: "KMUD",
+            name: "KMUD 88.1",
             logo: "stations/images-america/eureka/KMUD.png",
             url: "http://live.str3am.com:2470/live"
         },
         {
-            name: "KSLG",
+            name: "KVIP Radio 89.3 FM",
+            logo: "stations/images-america/burns/KVIP Radio 89.5 FM.png",
+            url: "http://sc1.christiannetcast.com:9112/;"
+        },
+        {
+            name: "KHSU 90.5",
+            logo: "stations/images-america/eureka/KHSU 90.5.png",
+            url: "http://peace.str3am.com:6650/"
+        },
+        {
+            name: "JPR News",
+            logo: "stations/images-america/c2c/JPR Rhythm & News.png",
+            url: "http://jpr.streamguys.org/jpr-news"
+        },
+        {
+            name: "KSLG 93.1",
             logo: "stations/images-america/eureka/KSLG.png",
             url: "http://lostcoast.streamguys.us/kslg-hi"
         },
         {
-            name: "99.1 Kiss FM",
-            logo: "stations/images-america/eureka/99.1 Kiss FM.png",
-            url: "http://r2.radioloyalty.net/lmjc2276?type=.mp3"
+            name: "94.1 The Lounge",
+            logo: "stations/images-america/eureka/94.1 The Lounge.png",
+            url: "http://lostcoast.streamguys.us/the_lounge"
         },
         {
-            name: "KWPT",
-            logo: "stations/images-america/eureka/KWPT.png",
-            url: "http://kwpt.com/stream"
+            name: "K-LOVE",
+            logo: "stations/images-america/huron/K-LOVE.png",
+            url: "http://emf.streamguys1.com/sk001_mp3_high_web?tsid=1516209902878"
         },
         {
-            name: "Humboldt 101",
-            logo: "stations/images-america/eureka/Humboldt 101.png",
-            url: "http://sky1.torontocast.com:9103/stream"
+            name: "KHUM 104.7",
+            logo: "stations/images-america/eureka/KHUM 104.7.svg",
+            url: "http://lostcoast.streamguys.us/khum-hi"
+        },
+        {
+            name: "KINS 106.3",
+            logo: "stations/images-america/eureka/KINS 106.3.png",
+            url: "http://eurekaradio.streamguys1.com/live"
         },
     ],
     "farmington": [
@@ -3806,22 +3826,40 @@ var stations = {
     ],
     "ukiah": [
         {
-            name: "The Coast",
-            logo: "stations/images-america/ukiah/The Coast.png",
-            url: "http://54.82.44.170/caradio-koztfmmp3-ibc3"
+            name: "Broken FM 88.7",
+            logo: "stations/images-america/ukiah/Broken FM.png",
+            url: "http://brokenfm.primcast.com:6834/;stream/1"
         },
         {
             name: "Family Radio West Coast",
             logo: "stations/images-america/ukiah/Family Radio West Coast.svg",
             url: "http://playerservices.streamtheworld.com/api/livestream-redirect/FAMILYRADIO_WEST.mp3"
         },
-        /*
         {
-            name: "The Skunk",
-            logo: "stations/images-america/ukiah/The Skunk.png",
-            url: "http://streaming.rubinbroadcasting.com:80/kunk"
+            name: "KZYX 91.5",
+            logo: "stations/images-america/ukiah/KZYZ 91.5.png",
+            url: "http://kzyx.mcn.org:4000/kzyxlive"
         },
-        */
+        {
+            name: "Max 93.5 FM",
+            logo: "stations/images-america/ukiah/Max 93.5 FM.jpg",
+            url: "http://crystalout.surfernetwork.com:8001/KMKX-FM_MP3"
+        },
+        {
+            name: "94.5 K-Wine",
+            logo: "stations/images-america/ukiah/94.5 K-Wine.jpg",
+            url: "http://crystalout.surfernetwork.com:8001/KWNE-FM_MP3"
+        },
+        {
+            name: "The Coast 95.3",
+            logo: "stations/images-america/ukiah/The Coast.png",
+            url: "http://54.82.44.170/caradio-koztfmmp3-ibc3"
+        },
+        {
+            name: "The Skunk FM 96.7",
+            logo: "stations/images-america/ukiah/The Skunk FM 96.7.png",
+            url: "http://streaming.rubinbroadcasting.com/kunk"
+        },
     ],
     "winnemucca": [
         {
