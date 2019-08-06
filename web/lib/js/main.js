ff8d602776aec1402184c55e6c396b695c6f44b2
Koen <koen@koenvh.nl>
2019-08-06 16:00:18 +0200

    Update Spanish

--- web/lib/js/main.js
+++ web/lib/js/main.js
@@ -338,6 +338,7 @@ function setRadioStation(url, country, volume) {
         $("#player").animate({volume: 0}, 2000, function () {
             //Detach previous HLS if it is there
             if (g_hls != null) {
+                g_hls.stopLoad();
                 g_hls.destroy();
                 g_hls = null;
             }
